ce867a65e5b891077ed56ea3ca8b7dbec94227bd
Ian Macartney <ianhmacartney@gmail.com>
2025-10-30 01:53:31 -0700

    move package to root

--- src/tiptap/index.ts
+++ src/tiptap/index.ts
@@ -113,7 +113,7 @@ export function syncExtension(
   opts?: UseSyncOptions
 ): AnyExtension {
   const log: typeof console.log = opts?.debug ? console.debug : () => {};
-  let snapshotTimer: NodeJS.Timeout | undefined;
+  let snapshotTimer: number | undefined;
   const trySubmitSnapshot = (version: number, content: string) => {
     if (snapshotTimer) {
       clearTimeout(snapshotTimer);
