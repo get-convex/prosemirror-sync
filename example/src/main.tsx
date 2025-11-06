436c1e046d9c6178e2e390e0e9902d323b04624c
Ian Macartney <ianhmacartney@gmail.com>
2025-11-05 23:33:57 -0800

    update codegen

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import App from "./App.tsx";
+import App from "./App.jsx";
 import "./index.css";
 
 const address = import.meta.env.VITE_CONVEX_URL;
